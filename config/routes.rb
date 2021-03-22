15c72ac70b5b0d8dee095e2d6d490483c5d44c8f
colonel_panic <colekraten@gmail.com>
2021-03-21 19:43:36 -0700

    added and configured GLR, bulma, and better_errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :yeets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "yeets#index"
 end
